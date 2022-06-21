961e056ea0fd5950b828e993c54da44b7dd3efb3
Alexander Danilov <h4rb4rd@gmail.com>
2022-06-22 00:32:41 +0300

    feat: add preloader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { RecoilRoot } from 'recoil'
+import NextNProgress from 'nextjs-progressbar'
 
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../hooks/useAuth'
@@ -7,11 +8,21 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<RecoilRoot>
-			<AuthProvider>
-				<Component {...pageProps} />
-			</AuthProvider>
-		</RecoilRoot>
+		<>
+			<RecoilRoot>
+				<AuthProvider>
+					<Component {...pageProps} />
+				</AuthProvider>
+			</RecoilRoot>
+			<NextNProgress
+				color='#E50914'
+				startPosition={0.3}
+				stopDelayMs={200}
+				height={3}
+				showOnShallow={true}
+				options={{ showSpinner: false }}
+			/>
+		</>
 	)
 }
 
